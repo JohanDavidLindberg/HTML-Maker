fe68b3aef36702325c248ba1f7f366b4d0653a20
JohanDavidLindberg <94990546+JohanDavidLindberg@users.noreply.github.com>
2022-05-27 20:03:40 +0200

    Funka

--- Görare.java
+++ Görare.java
@@ -1,4 +1,5 @@
 package HTML_Maker;
+
 import java.io.*;
 
 public class Görare{
@@ -10,11 +11,48 @@ public class Görare{
         this.fil = fil;
     }
 
+    public void start(String Titel) throws IOException  {
+        skrivare.append("<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<meta charset=\"UTF-8\">\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n<title>Document</title>\n</head>\n<body>");
+    }
+    public void slut() throws IOException  {
+        skrivare.append("</body>\n</html>");
+    }
+
     public void div(Object o) throws IOException {
-        skrivare.append("<div>" + o + "</div>");
+        skrivare.append("\n<div>" + o + "</div>\n");
     }
     public void div() throws IOException {
-        skrivare.append("<div></div>");
+        skrivare.append("\n<div></div>\n");
+    }
+    public void divid(Object o, String id) throws IOException {
+        skrivare.append("\n<div id=\"" + id + "\">" + o + "</div>\n");
+    }
+    public void divid(String id) throws IOException {
+        skrivare.append("\n<div id=\"" + id + "\"></div>\n");
+    }
+    public void h1(Object o) throws IOException {
+        skrivare.append("\n<h1>" + o + "</h1>\n");
+    }
+    public void h1() throws IOException {
+        skrivare.append("\n<h1></h1>\n");
+    }
+    public void h2(Object o) throws IOException {
+        skrivare.append("\n<h2>" + o + "</h2>\n");
+    }
+    public void h2() throws IOException {
+        skrivare.append("\n<h2></h2>\n");
+    }
+    public void p(Object o) throws IOException {
+        skrivare.append("\n<p>" + o + "</p>\n");
+    }
+    public void p() throws IOException {
+        skrivare.append("\n<p></p>\n");
+    }
+    public void li(Object o) throws IOException {
+        skrivare.append("\n<li>" + o + "</li>\n");
+    }
+    public void li() throws IOException {
+        skrivare.append("\n<li></li>\n");
     }
     public void close(){
         skrivare.close();
--- Main.java
+++ Main.java
@@ -5,17 +5,23 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         File fil = new File("index.html");
+        fil.delete();
         fil.createNewFile();
 
         FileWriter skrivare = new FileWriter("index.html", true);
 
-
-        skrivare.append("<div></div>");
-
         Görare html = new Görare(skrivare, fil);
 
-        html.div("hejsan");
+        html.start("Johan");
+        html.h1("Bästa sidan");
+        html.h2("Ålder");
+        html.li("18");
+        html.p("Johan är född 28 maj 2004 och är därmed 18 ");
+        html.divid(html.p("ojsan"), "div1");
+        html.slut();
 
         skrivare.close();
+
+
     }
 }
